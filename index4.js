402e0043937f8c9b1d4664049005aae2c196bbb7
shitanshu-277 <shitanshugoyal2002@gmail.com>
2023-10-24 22:34:58 +0530

    conflict resolve

--- index4.js
+++ index4.js
@@ -1,5 +1,13 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 hi how are you
+=======
+console.log("hi")
+>>>>>>> Stashed changes
+=======
+console.log("hi")
+>>>>>>> Stashed changes
 =======
 console.log("hi")
 >>>>>>> Stashed changes
